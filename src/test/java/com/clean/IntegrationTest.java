f1e7b56e1fd7cf9e9a4392d21c58e3a3be899a26
jaemin4 <eheh25877@gmail.com>
2025-04-11 01:52:32 +0900

    test : 결제 통합 테스트

--- src/test/java/com/clean/IntegrationTest.java
+++ src/test/java/com/clean/IntegrationTest.java
@@ -2,20 +2,25 @@ package com.clean;
 
 import com.clean.application.BalanceFrontService;
 import com.clean.application.OrderFrontService;
+import com.clean.application.PaymentFrontService;
 import com.clean.domain.balance.Balance;
 import com.clean.domain.balance.BalanceHistory;
 import com.clean.domain.order.Order;
+import com.clean.domain.order.OrderItem;
 import com.clean.domain.order.OrderService;
 import com.clean.domain.product.Product;
 import com.clean.domain.product.ProductService;
 import com.clean.infra.balance.BalanceHistoryLocalDatabase;
 import com.clean.infra.balance.BalanceLocalDatabase;
+import com.clean.infra.order.OrderLocalDatabase;
+import com.clean.infra.payment.PaymentHistoryLocalDatabase;
 import com.clean.infra.product.ProductLocalDatabase;
 import com.clean.interfaces.model.dto.req.ReqCalculateTotalAmountDto;
 import com.clean.interfaces.model.dto.res.ResCompleteOrderDto;
 import com.clean.interfaces.model.param.CompleteChargeParam;
 import com.clean.interfaces.model.param.OrderItemParam;
 import com.clean.interfaces.model.param.OrderParam;
+import com.clean.interfaces.model.param.PaymentParam;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -49,6 +54,12 @@ public class IntegrationTest {
     private OrderService orderService;
     @Autowired
     private ProductService productService;
+    @Autowired
+    private PaymentFrontService paymentFrontService;
+    @Autowired
+    private OrderLocalDatabase orderLocalDatabase;
+    @Autowired
+    private PaymentHistoryLocalDatabase paymentHistoryLocalDatabase;
 
     /*
   todo
@@ -131,6 +142,48 @@ public class IntegrationTest {
         assertEquals(expectedAmount, result.getTotalAmount());
         assertEquals(expectedOrderId, result.getOrderId());
     }
+
+    @DisplayName("주문 → 결제 → 잔액 차감 및 이력 저장까지 전체 플로우 검증")
+    @Test
+    void paymentCompleteTest() {
+        // todo 주문
+        Long paramUserId = 1L;
+        OrderParam orderParam = new OrderParam(paramUserId, List.of(
+                new OrderItemParam(1L, 1L),
+                new OrderItemParam(2L, 2L)
+        ));
+        ResCompleteOrderDto resCompleteOrderDto = orderFrontService.completeOrder(orderParam);
+        Long paramOrderId =  resCompleteOrderDto.getOrderId();
+        List<OrderItem> orderItems = resCompleteOrderDto.getItems();
+
+        // todo 결제
+        paymentFrontService.completePayment(new PaymentParam(paramUserId, paramOrderId));
+
+        // todo 주문에 해당하는 상품 가져와지는지
+        Order resOrder =  orderLocalDatabase.getOrderById(paramOrderId).orElseThrow();
+
+        // todo 총 결제 금액 계산
+        List<ReqCalculateTotalAmountDto> reqCalculateTotalAmountDtos = resCompleteOrderDto.getItems().stream()
+                .map(item -> new ReqCalculateTotalAmountDto(item.getProductId(), item.getQuantity()))
+                .toList();
+
+        long totalAmount = productService.calculateTotalAmount(reqCalculateTotalAmountDtos);
+
+        // todo 사용자 잔액 검증
+        Balance balance = balanceLocalDatabase.findByUserId(paramOrderId).orElseThrow();
+        assertEquals(100_000_000L - totalAmount, balance.getAmount());
+
+        // todo 결제 내역 검증
+        var payment = paymentHistoryLocalDatabase.findAll().get(0);
+        assertEquals(paramUserId, payment.getUserId());
+        assertEquals(-totalAmount, payment.getAmount());
+
+        // todo 잔액 사용 이력 검증
+        var balanceHistory = balanceHistoryLocalDatabase.findAll().get(0);
+        assertEquals(paramUserId, balanceHistory.getUserId());
+        assertEquals(-totalAmount, balanceHistory.getAmount());
+    }
+
 
 
 }
